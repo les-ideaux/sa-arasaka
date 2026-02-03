3c1ed1520432a69d40ff7cfb1e4ef080d4b02600
kyllian.mgn <shokkzu@gmail.com>
2026-02-03 15:14:59 +0100

    Merged local and remote changes

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,22 +1,15 @@
-import {Component, inject, signal} from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {HttpClient, provideHttpClient} from '@angular/common/http';
-import {environment} from '../environments/environment';
+import {Parking} from './parking/parking';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
+  imports: [
+    Parking
+  ],
   styleUrl: './app.scss'
 })
 export class App {
   protected readonly title = signal('Parking');
-
-  httpClient = inject(HttpClient)
-
-  onButtonClick(){
-    console.log("I've been clicked!")
-    console.log(import.meta.env)
-    this.httpClient.get(`${import.meta.env.NG_APP_API_URL}/hello`).subscribe(data => console.log(data));
-    this.httpClient.get(`${import.meta.env.NG_APP_API_URL}/sendTestEmail`).subscribe(data => console.log(data));
-  }
 }
